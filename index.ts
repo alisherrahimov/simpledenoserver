5d9a0beff648fc5acc2c7cbbe9bfa2cd5d9c9838
Alisher <49154018+alisherrahimov@users.noreply.github.com>
2022-05-27 16:03:53 +0500

    a

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import { connectDB } from "./db.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import { Application } from "https://deno.land/x/oak/mod.ts";
-import { Bot } from "https://deno.land/x/telegram@v0.1.1/mod.ts";
+import { Bot, Telegram } from "https://deno.land/x/telegram@v0.1.1/mod.ts";
 const bot = new Bot("5259090349:AAHQJzbGx-_mE1dmHec1IbP8F97t2ByIdXk");
-
+bot.launch({
+  webhook: {
+    domain: "https://simpledenoserver.deno.dev",
+    path: "/",
+    port: 5000,
+  },
+});
 bot.on("message", async (ctx) => {
   if ("text" == ctx.message?.text) {
     ctx.reply("asdasdas");
